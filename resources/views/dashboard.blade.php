1527a10696414609887f974d15cddfd4dd9173ac
mehmetgundur <mehmetgundur@hotmail.com>
2022-01-21 00:43:19 +0300

    Bugünlük bu kadar.

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,5 @@
+<x-app-layout>
+    <x-slot name="header">Anasayfa</x-slot>
+
+    <div class="aler alert-danger">Bootstrap alert mesajı.</div>
+</x-app-layout>
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,17 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
+    return view('dashboard');
+})->name('dashboard');
+
+Route::group(['middleware' =>['auth','isAdmin'],'prefix'=>'admin'],function (){
+    Route::get('/deneme',function (){
+       return 'Yetkili kullanıcı';
+    });
+
+    Route::get('/test',function (){
+        return 'Test123';
+    });
+});
